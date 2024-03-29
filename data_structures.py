778e630feadea398dac99fb4228ca9d74bd2ec5e
Anastasia Dvorova <143041035+7dvorovanastya@users.noreply.github.com>
2024-03-30 01:48:22 +0200

    Resolved security vulnerability in authentication system

---
+++ data_structures.py
@@ -0,0 +1,2 @@
+styleUpdate(w2Style, _pFgColour[1], _pBgColour[1],
+I see two ways of how we could make errors of this type fewer. First, we should use static code analyzers. They allow us to detect many errors of this class at the very early stages. It is cheaper and easier to find and fix an error right after writing the code than handle the same error detected during the testing.
